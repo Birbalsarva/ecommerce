cb1a69410aa390233267ef8f204defd5dec527f5
Birbal S Naik <112779904+Birbalsarva@users.noreply.github.com>
2024-04-05 16:29:38 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,26 @@
 
+# main.tf
+
+# EC2 instance
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-080e1f13689e07408"  
+  instance_type = "t2.medium"
+  tags = {
+    Name = "ecommerce-ec2-instance"
+  }
+}
+
+# S3 bucket
+resource "aws_s3_bucket" "ecommerce_bucket" {
+  bucket = "ecommerce-bucket-name"  
+  acl    = "private"
+}
+
+# MongoDB instance
+resource "aws_instance" "mongodb_instance" {
+  ami           = "ami-080e1f13689e07408"  
+  instance_type = "t2.medium"
+  tags = {
+    Name = "ecommerce-mongodb-instance"
+  }
+}
